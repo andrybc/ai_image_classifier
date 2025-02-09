75e92061aade29f7cce30b1ebe5f5f61e567567c
andrybc <canelbandry@gmail.com>
2025-02-09 12:43:31 -0500

    printing to error log

--- api/db_connection.php
+++ api/db_connection.php
@@ -9,6 +9,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    error_log("Database connection failed: " . $conn->connect_error);
+    die("Database connection failed. Check the error log.");
+} else {
+    error_log("Database connected successfully.");
 }
 ?>
